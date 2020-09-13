7b0028d3218a48ffb7e37b1b62b0d8b5f54a84ed
unknown <jose@pippa.com.br>
2020-09-13 16:42:55 -0300

    Updates

---
+++ precache-manifest.09806a2b36a84567748d15a7320eb4e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f66e9ede9b666ed657c9cd6553dbfe9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5d9e97c6204c04dfbd92",
+    "url": "/monsters-rolodex/static/css/main.141d7862.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9e97c6204c04dfbd92",
+    "url": "/monsters-rolodex/static/js/main.a0c5eeff.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
